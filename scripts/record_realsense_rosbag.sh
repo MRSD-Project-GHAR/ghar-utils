0ab99828d931f1b4dbeb8120cae1a2b95443440e
Madhu Korada <manoj.korada0000@gmail.com>
2023-04-02 23:23:50 +0000

    Refined record script

--- scripts/record_realsense_rosbag.sh
+++ scripts/record_realsense_rosbag.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+gnome-termial
+cd ~/ghar/kimera_ws
+
+# Kill previous session
+tmux kill-session
+
 # Create a new tmux session
 session_name="record_bag_$(date +%s)"
 tmux new-session -d -s $session_name
@@ -13,17 +19,18 @@ tmux splitw -h -p 50 # split it into two halves
 # Run the roslaunch command in the first pane
 tmux select-pane -t 0
 tmux send-keys "rosrun dynamic_reconfigure dynparam set /camera/stereo_module emitter_enabled 0" Enter
+tmux send-keys "sleep 3 && roslaunch realsense2_camera rs_camera_ghar.launch" Enter
 
 # Run the teleop.py script in the second pane
 tmux select-pane -t 1
-tmux send-keys "roslaunch realsense2_camera rs_camera_ghar.launch" Enter
+tmux send-keys 'sleep 10 && rosbag record -a -x "/(.*)/theora/(.*)|(.*)/theora|/(.*)/compressed/(.*)|(.*)/compressed|/(.*)/compressedDepth/(.*)|(.*)/compressedDepth"' Enter
 
 # Change the directory to ../topomaps/bags and run the rosbag record command in the third pane
 tmux select-pane -t 2
-tmux send-keys 'rosbag record -a -O data.bag -x "/(.*)/theora/(.*)|(.*)/theora|/(.*)/compressed/(.*)|(.*)/compressed|/(.*)/compressedDepth/(.*)|(.*)/compressedDepth"' Enter
+tmux send-keys "roslaunch interbotix_xslocobot_control xslocobot_control.launch robot_model:=locobot_wx200 use_base:=true use_camera:=false use_lidar:=false" Enter
 
 tmux select-pane -t 3
-
+tmux send-keys "roslaunch kobuki_keyop keyop.launch __ns:=locobot" Enter
 
 # Attach to the tmux session
 tmux -2 attach-session -t $session_name
